27afb32391e72fb3f1e605008fded1d3a7d4008d
Graham Greene <123007914+grahamgreene88@users.noreply.github.com>
2024-01-26 19:37:33 -0500

    Converted json to dataframe in API call.

--- AQ_API_script.py
+++ AQ_API_script.py
@@ -2,7 +2,9 @@ import requests
 import json
 import pandas as pd
 
-url = "https://api.openaq.org/v2/measurements?location_id=498&parameter=o3&parameter=pm25&date_from=2023-12-23T01:20:56-05:00&date_to=2023-12-24T01:20:56-05:00&limit=1000"
+#pd.set_option("display.max_colwidth", None)
+
+url = "https://api.openaq.org/v2/latest/498?limit=100&page=1&offset=0&sort=asc"
 
 headers = {
     "X-API-Key": "5eb79066433eb416b9a688101dfba9b40f44ffbbd58f913d2f8aafd4867395e5",
@@ -18,11 +20,16 @@ try:
 except(ConnectionError, TimeoutError, HTTPError) as e:
     print(e)
 
-df = pd.json_normalize(data['results'])
-pd.set_option('display.max_columns', None)
-print(df)
+results_df = pd.json_normalize(data['results'], record_path=['measurements'])
+print(results_df)
 # # if response.status_code == 200:
     
 # print(f"Status Code: {response.status_code}")
 # print(response.text)
 # response.raise_for_status(HTTPError)
+
+
+
+# response = requests.get(url, headers=headers)
+
+# print(response.text)
